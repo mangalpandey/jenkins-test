1d73843b88ac037383e1a8ac6871ecf7aee8ff30
mangal-yuwee <mangal.pandey@yuwee.com>
2021-01-25 11:21:00 +0530

    test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,7 +40,7 @@ public class TestGreeter {
 
    @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "Chant Hare Krishna and be happy";
+    String someone = "Chant Hare Krishna and be happy with family";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
